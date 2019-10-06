b374bd2c44afccd7fd1435a28959ec0f12e84cc7
John Hu <huziyong@gmail.com>
2019-10-06 23:26:19 +0800

    Add book-list component for shelf page
    - references imageslr/taro-library

--- src/actions/BookActionTypes.js
+++
@@ -1,7 +0,0 @@
-const BookActionTypes = {
-  FETCH_BOOKS: 'FETCH_BOOKS',
-  ADD_BOOK: 'ADD_BOOK',
-  DELETE_BOOK: 'DELETE_BOOK'
-};
-
-export default BookActionTypes;
--- src/pages/shelf/index.jsx
+++ src/pages/shelf/index.jsx
@@ -1,15 +1,26 @@
 import Taro, { Component } from '@tarojs/taro'
-import { View, Text } from '@tarojs/components'
+import { View } from '@tarojs/components'
+
+import API from '../../services/api';
+import Panel from '../../components/panel';
+import HorizonList from '../../components/horizon-list';
+import URL from "../../constants/urls";
 
 export default class Shelf extends Component {
 
-  config = {
-    navigationBarTitleText: '书架'
+  state = {
+    books: []
   }
 
-  componentWillMount () { }
+  componentWillMount () {
+    API.get('/books')
+       .then(res =>
+            this.setState({books: res.data})
+            )
+  }
 
-  componentDidMount () { }
+  componentDidMount () { 
+  }
 
   componentWillUnmount () { }
 
@@ -17,10 +28,20 @@ export default class Shelf extends Component {
 
   componentDidHide () { }
 
+  config = {
+    navigationBarTitleText: '书架'
+  }
+
   render () {
     return (
       <View className='shelf'>
-        <Text>Hello world!</Text>
+        <Panel
+          url={`${URL.BOOK_LIST}?type=new`}
+          title='新书速递'
+          className='panel--first'
+        >
+          <HorizonList data={this.state.books} />
+        </Panel>
       </View>
     )
   }
