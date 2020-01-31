fcb1fd9eb948a1639eaf8bc14dd3a6ba09c06abc
John Hu <huziyong@gmail.com>
2020-01-31 19:04:37 +0800

    Remove redux for no-use

--- src/services/config.js
+++ src/services/config.js
@@ -1,5 +1,5 @@
 const devURL = "http://localhost:3000/api/v1";
-const prodURL = "http://bookcross.herokuapp.com/api/v1";
+const prodURL = "https://zhimaishu.com/api/v1";
 const BASE_URL = process.env.NODE_ENV === "development" ? devURL : prodURL;
 
 export default BASE_URL;
